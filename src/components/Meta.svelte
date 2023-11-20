092a3c4ce6b24761c9ff784113e2e548341e3897
russellsamora <russellsamora@gmail.com>
2023-11-20 14:50:34 -0500

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -19,8 +19,8 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:locale" content="en_US" />
 
-	<meta property="og:image" content="{url}/assets/social-facebook.jpg" />
-	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image" content="{url}/assets/social.png" />
+	<meta property="og:image:type" content="image/png" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="600" />
 
@@ -29,11 +29,30 @@
 	<meta name="twitter:creator" content="@puddingviz" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image:src" content="{url}/assets/social-twitter.jpg" />
+	<meta name="twitter:image:src" content="{url}/assets/social.png" />
 
 	<meta name="robots" content="max-image-preview:large" />
 
 	<link rel="canonical" href="{url}/" />
+
+	<link
+		rel="apple-touch-icon"
+		sizes="180x180"
+		href="assets/apple-touch-icon.png"
+	/>
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="32x32"
+		href="assets/favicon-32x32.png"
+	/>
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="16x16"
+		href="assets/favicon-16x16.png"
+	/>
+	<link rel="manifest" href="assets/site.webmanifest" />
 
 	{#each preloadFont as href}
 		<link rel="preload" {href} as="font" type="font/woff2" crossorigin />
