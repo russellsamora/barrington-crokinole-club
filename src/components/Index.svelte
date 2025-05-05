2041d5b65e87a4b0ace58777a6d26546e024a8bb
Russell Samora <russellsamora@gmail.com>
2025-05-05 10:10:17 -0400

    update github pages

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -14,7 +14,7 @@
 
 	<p>
 		<small>Next Event</small><br />
-		<strong>Monday, Apr. 28 @ 7:30 &mdash; The Prairie Whale</strong>
+		<strong>Monday, May 5 @ 7:30 &mdash; The Prairie Whale</strong>
 		<!-- <strong>TBD</strong> -->
 	</p>
 
